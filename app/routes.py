46901e3bed2639e489aa6f572b1bcd678386a924
Kauan <jailma.kmp@gmail.com>
2022-09-16 11:18:44 -0300

    [feat] page navigation between new and old posts

--- app/routes.py
+++ app/routes.py
@@ -29,18 +29,32 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(                         # page arg == what page i'm?
         page, app.config['POSTS_PER_PAGE'], False)                          # app.config arg == how much posts per page i have
-                                                                            # False arg is to not return 404 error when page is out of range
+                                                                            # False arg is to not return 404 error when page is out of range    
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+
     return render_template('index.html', title='Home', form=form,
-                           posts=posts.items)
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
+
 
 @app.route('/explore')
 @login_required
 def explore():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(            # same args than index page       
-        page, app.config['POSTS_PER_PAGE'], False)                          
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(            # same args than index page  
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
 
-    return render_template("index.html", title='Explore', posts=posts.items)
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+
+    return render_template("index.html", title='Explore', posts=posts.items,
+                          next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
